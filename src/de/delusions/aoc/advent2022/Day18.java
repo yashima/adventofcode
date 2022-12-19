8af4e08e8eba0d3ca6dbc87c74f322e1c1e0926f
Sonja Pieper <yashima@users.noreply.github.com>
2022-12-20 00:39:34 +0100

    Day18 + 19 preparation--I am lagging behind. Too much numbers juggling.

---
+++ src/de/delusions/aoc/advent2022/Day18.java
@@ -0,0 +1,23 @@
+package de.delusions.aoc.advent2022;
+
+import de.delusions.aoc.util.Day;
+
+import java.util.stream.Stream;
+
+public class Day18
+    extends Day<Integer> {
+
+    Day18(){
+        super(18,"Boiling Boulders");
+    }
+
+    @Override
+    public Integer part1( Stream<String> input ) {
+        return null;
+    }
+
+    @Override
+    public Integer part2( Stream<String> input ) {
+        return null;
+    }
+}
---
+++ src/de/delusions/aoc/advent2022/Day19.java
@@ -0,0 +1,22 @@
+package de.delusions.aoc.advent2022;
+
+import de.delusions.aoc.util.Day;
+
+import java.util.stream.Stream;
+
+public class Day19 extends Day<Integer> {
+
+    public Day19() {
+        super( 19, "Not Enough Minerals" );
+    }
+
+    @Override
+    public Integer part1( Stream<String> input ) {
+        return null;
+    }
+
+    @Override
+    public Integer part2( Stream<String> input ) {
+        return null;
+    }
+}
