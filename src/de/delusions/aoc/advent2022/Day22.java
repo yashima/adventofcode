ebd37c9d2b4b69e0ab792814423902a96d337252
Sonja Pieper <yashima@users.noreply.github.com>
2022-12-23 13:34:37 +0100

    Day22: just added day and input.

---
+++ src/de/delusions/aoc/advent2022/Day22.java
@@ -0,0 +1,21 @@
+package de.delusions.aoc.advent2022;
+
+import de.delusions.aoc.util.Day;
+
+import java.util.stream.Stream;
+
+public class Day22 extends Day<Integer> {
+    public Day22() {
+        super( 22, "Monkey Map" );
+    }
+
+    @Override
+    public Integer part1( Stream<String> input ) {
+        return null;
+    }
+
+    @Override
+    public Integer part2( Stream<String> input ) {
+        return null;
+    }
+}
