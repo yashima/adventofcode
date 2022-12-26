e0be180381031fc64ef3dcae63a08210995e98ce
Sonja Pieper <yashima@users.noreply.github.com>
2022-12-26 10:52:53 +0100

    Day 20: part 1 example works

--- src/de/delusions/aoc/advent2022/AdventOfCode2022.java
+++ src/de/delusions/aoc/advent2022/AdventOfCode2022.java
@@ -11,7 +11,7 @@ public class AdventOfCode2022 {
             List.of( new Day1(), new Day2(), new Day3(), new Day4(), new Day5(), new Day6(), new Day7(), new Day8(), new Day9(), new Day10(),
                      new Day11(), new Day12(), new Day13(), new Day14(), new Day15(), new Day16(), new Day17() , new Day18());
         System.out.println( "Days solved: " + daysOfAdvent.size() );
-        Day<?> today = new Day19();
+        Day<?> today = new Day20();
         today.run( true, true );
 
 
--- src/de/delusions/aoc/advent2022/Day20.java
+++ src/de/delusions/aoc/advent2022/Day20.java
@@ -2,29 +2,82 @@ package de.delusions.aoc.advent2022;
 
 import de.delusions.aoc.util.Day;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class Day20 extends Day<Integer> {
 
-    List<Integer> numbers;
+    AtomicInteger ID = new AtomicInteger( 0 );
 
-    public Day20( ) {
+    LinkedList<CryptPair> numbers;
+
+    Integer ZERO = null;
+
+    public Day20() {
         super( 20, "Grove Positioning System" );
     }
 
     @Override
     public Integer part1( Stream<String> input ) {
-        numbers = parse( input );
-        return null;
+        numbers = new LinkedList<>( parse( input ) );
+        CryptPair nextPair = getNext();
+        while ( nextPair != null ) {
+            movePair( nextPair );
+            nextPair = getNext();
+            System.out.println( numbers );
+        }
+        return getValueAtPosition( 1000 ) + getValueAtPosition( 2000 ) + getValueAtPosition( 3000 );
     }
+
+    int getZeroPosition() {
+        if ( ZERO == null ) {
+            CryptPair zero = numbers.stream().filter( p -> p.number == 0 ).findFirst().get();
+            ZERO = numbers.indexOf( zero );
+        }
+        return ZERO;
+    }
+
+    int getValueAtPosition( int index ) {
+        return numbers.get( ( index + getZeroPosition() ) % ( numbers.size() ) ).number;
+    }
+
+    private void movePair( CryptPair nextPair ) {
+        int index = numbers.indexOf( nextPair );
+        int newIndex = index + nextPair.number;
+        if ( newIndex < 0 ) {
+            newIndex = numbers.size() + newIndex - 1;
+        }
+        else if ( newIndex > numbers.size() ) {
+            newIndex = newIndex % numbers.size() + 1;
+        }
+        else if ( newIndex == 0 && index != 0 ) {
+            newIndex = numbers.size() - 1;
+        }
+        numbers.remove( index );
+        numbers.add( newIndex, new CryptPair( nextPair.id, nextPair.number, true ) );
+    }
+
+    private CryptPair getNext() {
+        return numbers.stream().filter( Predicate.not( CryptPair::moved ) ).findFirst().orElse( null );
+    }
+
+    List<CryptPair> parse( Stream<String> input ) {
+        return input.map( line -> new CryptPair( ID.getAndIncrement(), Integer.parseInt( line ), false ) ).toList();
+    }
+
 
     @Override
     public Integer part2( Stream<String> input ) {
         return null;
     }
 
-    List<Integer> parse(Stream<String> input){
-        return input.map( line -> Integer.parseInt( line ) ).toList();
+    record CryptPair(int id, int number, boolean moved) {
+        @Override
+        public String toString() {
+            return number + "";
+        }
     }
 }
