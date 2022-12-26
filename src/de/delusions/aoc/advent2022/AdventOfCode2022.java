d7b7f0d12cb72c6f3c909e984da72fb722df17ab
Sonja Pieper <yashima@users.noreply.github.com>
2022-12-26 15:01:18 +0100

    Day 20: trying to fix this by writing unit tests.

--- src/de/delusions/aoc/advent2022/AdventOfCode2022.java
+++ src/de/delusions/aoc/advent2022/AdventOfCode2022.java
@@ -12,7 +12,7 @@ public class AdventOfCode2022 {
                      new Day11(), new Day12(), new Day13(), new Day14(), new Day15(), new Day16(), new Day17() , new Day18());
         System.out.println( "Days solved: " + daysOfAdvent.size() );
         Day<?> today = new Day20();
-        today.run( true, true );
+        today.run( false, true );
 
 
     }
--- src/de/delusions/aoc/advent2022/Day20.java
+++ src/de/delusions/aoc/advent2022/Day20.java
@@ -10,7 +10,7 @@ import java.util.stream.Stream;
 
 public class Day20 extends Day<Integer> {
 
-    AtomicInteger ID = new AtomicInteger( 0 );
+    static AtomicInteger ID = new AtomicInteger();
 
     LinkedList<CryptPair> numbers;
 
@@ -20,16 +20,12 @@ public class Day20 extends Day<Integer> {
         super( 20, "Grove Positioning System" );
     }
 
-    @Override
-    public Integer part1( Stream<String> input ) {
-        numbers = new LinkedList<>( parse( input ) );
-        CryptPair nextPair = getNext();
-        while ( nextPair != null ) {
-            movePair( nextPair );
-            nextPair = getNext();
-            System.out.println( numbers );
-        }
-        return getValueAtPosition( 1000 ) + getValueAtPosition( 2000 ) + getValueAtPosition( 3000 );
+    static CryptPair newPair( int number ) {
+        return new CryptPair( ID.getAndIncrement(), number, false );
+    }
+
+    static CryptPair movedPair( CryptPair pair ) {
+        return new CryptPair( pair.id, pair.number, true );
     }
 
     int getZeroPosition() {
@@ -44,30 +40,57 @@ public class Day20 extends Day<Integer> {
         return numbers.get( ( index + getZeroPosition() ) % ( numbers.size() ) ).number;
     }
 
-    private void movePair( CryptPair nextPair ) {
-        int index = numbers.indexOf( nextPair );
-        int newIndex = index + nextPair.number;
-        if ( newIndex < 0 ) {
-            newIndex = numbers.size() + newIndex - 1;
+    //14306 too low
+    @Override
+    public Integer part1( Stream<String> input ) {
+        numbers = new LinkedList<>( parse( input ) );
+        mixNumbers();
+        System.out.println( numbers );
+        return getValueAtPosition( 1000 ) + getValueAtPosition( 2000 ) + getValueAtPosition( 3000 );
+    }
+
+    void mixNumbers() {
+        CryptPair nextPair = getNext();
+        while ( nextPair != null ) {
+            movePair( nextPair );
+            nextPair = getNext();
         }
-        else if ( newIndex > numbers.size() ) {
-            newIndex = newIndex % numbers.size() + 1;
-        }
-        else if ( newIndex == 0 && index != 0 ) {
-            newIndex = numbers.size() - 1;
-        }
-        numbers.remove( index );
-        numbers.add( newIndex, new CryptPair( nextPair.id, nextPair.number, true ) );
     }
 
     private CryptPair getNext() {
         return numbers.stream().filter( Predicate.not( CryptPair::moved ) ).findFirst().orElse( null );
     }
 
-    List<CryptPair> parse( Stream<String> input ) {
-        return input.map( line -> new CryptPair( ID.getAndIncrement(), Integer.parseInt( line ), false ) ).toList();
+    int movePair( CryptPair nextPair ) {
+        int index = numbers.indexOf( nextPair );
+        int newIndex = calculateNewIndex( nextPair, index );
+        numbers.remove( index );
+        numbers.add( newIndex, movedPair( nextPair ) );
+        return newIndex;
     }
 
+    int calculateNewIndex( CryptPair nextPair, int index ) {
+        int newIndex = index + nextPair.number; //simple sum suffices for many non-loops
+        int numbersWitoutAlice = numbers.size() - 1;
+        if ( newIndex < 0 ) { //step left
+            newIndex = numbersWitoutAlice + newIndex % numbersWitoutAlice;
+        }
+        else if ( newIndex > numbersWitoutAlice ) { //loop right
+            newIndex = nextPair.number() % numbersWitoutAlice;
+        }
+        else if ( newIndex == 0 && index != 0 ) { //the edge goes to the end
+            newIndex = numbersWitoutAlice;
+        }
+        return newIndex;
+    }
+
+    CryptPair getById( int id ) {
+        return numbers.stream().filter( cp -> cp.id == id ).findFirst().orElse( null );
+    }
+
+    List<CryptPair> parse( Stream<String> input ) {
+        return input.map( line -> newPair( Integer.parseInt( line ) ) ).toList();
+    }
 
     @Override
     public Integer part2( Stream<String> input ) {
