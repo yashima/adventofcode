2e88ef11d16397457b8e6538d1d7e1fd7109cb9c
Sonja Pieper <yashima@users.noreply.github.com>
2023-12-04 16:27:30 +0100

    Day 5: initialized.

---
+++ src/de/delusions/aoc/Day05.java
@@ -0,0 +1,21 @@
+package de.delusions.aoc;
+
+import de.delusions.util.Day;
+
+import java.util.stream.Stream;
+
+public class Day05 extends Day<Integer> {
+    public Day05( Integer... expected ) {
+        super( 5, "", expected );
+    }
+
+    @Override
+    public Integer part0( Stream<String> input ) {
+        return null;
+    }
+
+    @Override
+    public Integer part1( Stream<String> input ) {
+        return null;
+    }
+}
