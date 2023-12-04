1e4d5d01fdce9657bc52c0bb315fa7f10288a491
Sonja Pieper <yashima@users.noreply.github.com>
2023-12-04 09:38:21 +0100

    Day 4: Solution Part 1 this was the most hacky fun so far. I am not sure those this is helping with the next part though?

--- src/de/delusions/aoc/AdventOfCode2023.java
+++ src/de/delusions/aoc/AdventOfCode2023.java
@@ -11,7 +11,7 @@ public class AdventOfCode2023 {
                                              new Day01( 142L, 281L, 54561L, 54076L ), //
                                              new Day02( 8L, 2286L, 2600L, 86036L ), //
                                              new Day03( 4361, 467835, 556057, 82824352 ),//
-                                             new Day04( 0, 0, 0, 0 ) );
+                                             new Day04( 13, 30, 22488, 0 ) );
         System.out.println( "Days: " + daysOfAdvent.size() );
         runAllVariants( daysOfAdvent.get( daysOfAdvent.size() - 1 ) );
 
@@ -20,7 +20,7 @@ public class AdventOfCode2023 {
     private static void runAllVariants( Day<?> today ) {
         today.run( true, 0 );
         today.run( false, 0 );
-        today.run( true, 1 );
-        today.run( false, 1 );
+//        today.run( true, 1 );
+//        today.run( false, 1 );
     }
 }
--- src/de/delusions/aoc/Day04.java
+++ src/de/delusions/aoc/Day04.java
@@ -2,16 +2,29 @@ package de.delusions.aoc;
 
 import de.delusions.util.Day;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Day04 extends Day<Integer> {
     public Day04( Integer... expected ) {
-        super( 4, "todo", expected );
+        super( 4, "Scratchcards", expected );
     }
 
     @Override
     public Integer part0( Stream<String> input ) {
-        return null;
+        return input.map( this::countWinningNumbers ).mapToInt( n -> n == 0 ? 0 : (int) Math.pow( 2, n - 1 ) ).sum();
+    }
+
+    private int countWinningNumbers( String scratchCard ) {
+        //stupid single digits. Apparently there are no triple digits or else...
+        scratchCard = scratchCard.replaceAll( "  ", " 0" );
+        String winnersRegex = scratchCard.substring( scratchCard.indexOf( ":" ) + 1, scratchCard.indexOf( "|" ) ).trim().replace( ' ', '|' );
+        String numbers = scratchCard.substring( scratchCard.indexOf( "|" ) );
+        Matcher matcher = Pattern.compile( winnersRegex ).matcher( numbers );
+        int count = 0;
+        while ( matcher.find() ) {count++;}
+        return count;
     }
 
     @Override
