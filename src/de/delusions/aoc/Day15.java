84207b9047db277997c8b4357b6224be01b96680
Sonja Pieper <yashima@users.noreply.github.com>
2023-12-15 21:41:45 +0100

    Day 15: adding some more javadocs

--- src/de/delusions/aoc/Day15.java
+++ src/de/delusions/aoc/Day15.java
@@ -24,6 +24,7 @@ public class Day15 extends Day<Integer> {
 
     @Override
     public Integer part0( Stream<String> input ) {
+        //just a test of the hash function
         return Arrays.stream( input.collect( Collectors.joining() ).split( "," ) ).mapToInt( Day15::hashString ).sum();
     }
 
@@ -39,12 +40,23 @@ public class Day15 extends Day<Integer> {
 
     @Override
     public Integer part1( Stream<String> input ) {
-        bucketMap.clear();
+        bucketMap.clear(); //stupidest bug ever to forget to reset the class variable between runs
+
         //processing the input, creating the instructions and executing them to build the bucketMap:
         Arrays.stream( input.collect( Collectors.joining() ).split( "," ) ).map( LensInstruction::create ).forEach( this::executeInstruction );
-        return bucketMap.values().stream().mapToInt( bucket -> bucket.stream().mapToInt( lens -> power( bucket, lens ) ).sum() ).sum();
+
+        //calculating the power of the bucketMap, bucket by bucket, lense by lense
+        return bucketMap.values()
+                        .stream()
+                        .mapToInt( bucket -> bucket.stream().mapToInt( lens -> power( bucket.indexOf( lens ), lens ) ).sum() )
+                        .sum();
     }
 
+    /**
+     * Take an instruction and execute the command which either removes a lense from a bucket, adds a lense to a bucket or replaces one
+     *
+     * @param instruction the instruction to execute
+     */
     void executeInstruction( LensInstruction instruction ) {
         List<LensInstruction> bucket = bucketMap.getOrDefault( instruction.hash(), new ArrayList<>() );
         int index = bucket.indexOf( instruction );
@@ -65,12 +77,24 @@ public class Day15 extends Day<Integer> {
         }
     }
 
-    private static int power( List<LensInstruction> bucket, LensInstruction lens ) {
-        int bucketNumber = lens.hash() + 1;
-        int bucketSpot = bucket.indexOf( lens ) + 1;
-        return bucketNumber * bucketSpot * lens.focalLength();
+    /**
+     * Calculates the power of a given lens in a bucket
+     *
+     * @param position position in the bucket (positions start counting at 0, because java lists)
+     * @param lens     the lense provides its focal length and the hash
+     * @return the formula is the product of the hash+1, the focallength and the position in the bucket
+     */
+    private static int power( int position, LensInstruction lens ) {
+        return ( lens.hash() + 1 ) * ( position + 1 ) * lens.focalLength();
     }
 
+    /**
+     * Each instruction is immutable and contains several fields
+     * @param label the label which has to be unique in each bucket
+     * @param hash the hash calculated from the label which determines the bucket
+     * @param op the operation '=' for adding or replacing a lense or '-' for removing one
+     * @param focalLength the focallength is needed to calculate the power
+     */
     record LensInstruction(String label, int hash, char op, int focalLength) {
         static LensInstruction create( String instruction ) {
             Matcher m = p.matcher( instruction );
