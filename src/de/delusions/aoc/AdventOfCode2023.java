3d90d4d3de183e3c197aa008ec2f48ad66ee9223
Sonja Pieper <yashima@users.noreply.github.com>
2023-12-16 09:55:57 +0100

    Day 16: prep + input

--- src/de/delusions/aoc/AdventOfCode2023.java
+++ src/de/delusions/aoc/AdventOfCode2023.java
@@ -21,8 +21,7 @@ public class AdventOfCode2023 {
                                              new Day11( 374L, 8410L, 9543156L, 625243292686L ),
                                              new Day12( 21, 525152, 7622, 0 ),
                                              new Day13( 405, 400, 31739, 31539 ),
-                                             new Day14( 136, 64, 108759, 89089 ),
-                                             new Day15( 1320, 145, 503487, 261505 )
+                                             new Day14( 136, 64, 108759, 89089 ), new Day15( 1320, 145, 503487, 261505 ), new Day16( 0, 0, 0, 0 )
                                              //261686 too high
         );
         System.out.println( "Days: " + daysOfAdvent.size() );
---
+++ src/de/delusions/aoc/Day16.java
@@ -0,0 +1,21 @@
+package de.delusions.aoc;
+
+import de.delusions.util.Day;
+
+import java.util.stream.Stream;
+
+public class Day16 extends Day<Integer> {
+    public Day16( Integer... expected ) {
+        super( 16, "The Floor will be Lava", expected );
+    }
+
+    @Override
+    public Integer part0( Stream<String> input ) {
+        return null;
+    }
+
+    @Override
+    public Integer part1( Stream<String> input ) {
+        return null;
+    }
+}
