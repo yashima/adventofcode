03879e3aa954291566d9cc7bb8cbbf5660a1b8e8
Sonja Pieper <yashima@users.noreply.github.com>
2023-12-17 01:24:55 +0100

    Day 17: prep

---
+++ src/de/delusions/aoc/Day17.java
@@ -0,0 +1,21 @@
+package de.delusions.aoc;
+
+import de.delusions.util.Day;
+
+import java.util.stream.Stream;
+
+public class Day17 extends Day<Integer> {
+    public Day17( Integer... expected ) {
+        super( 17, "TODO", expected );
+    }
+
+    @Override
+    public Integer part0( Stream<String> input ) {
+        return null;
+    }
+
+    @Override
+    public Integer part1( Stream<String> input ) {
+        return null;
+    }
+}
