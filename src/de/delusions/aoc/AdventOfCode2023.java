0c190abf408595693634593cfe7d1e7ff8b8048c
Sonja Pieper <yashima@users.noreply.github.com>
2023-12-19 20:36:04 +0100

    Day 18: part 2 solved, no floodfill needed. I learned some neat math today.

--- src/de/delusions/aoc/AdventOfCode2023.java
+++ src/de/delusions/aoc/AdventOfCode2023.java
@@ -24,7 +24,7 @@ public class AdventOfCode2023 {
                                              new Day14( 136, 64, 108759, 89089 ),
                                              new Day15( 1320, 145, 503487, 261505 ), new Day16( 46, 51, 6921, 7594 ),//
                                              new Day17( 102, 71, 698, 825 ),//
-                                             new Day18( 62, 0, 0, 0 ) );
+                                             new Day18( "62", "952408144115", "58550", "47452118468566" ) );
         System.out.println( "Days: " + daysOfAdvent.size() );
         runAllVariants( daysOfAdvent.getLast() );
 
@@ -33,7 +33,7 @@ public class AdventOfCode2023 {
     private static void runAllVariants( Day<?> today ) {
         today.run( true, 0 );
         today.run( false, 0 );
-//        today.run( true, 1 );
-//        today.run( false, 1 );
+        today.run( true, 1 );
+        today.run( false, 1 );
     }
 }
--- src/de/delusions/aoc/Day18.java
+++ src/de/delusions/aoc/Day18.java
@@ -5,65 +5,108 @@ import de.delusions.util.Day;
 import de.delusions.util.Direction;
 import de.delusions.util.Matrix;
 
-import java.util.ArrayList;
+import java.math.BigInteger;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.Stack;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class Day18 extends Day<Integer> {
-    static final char OUTSIDE = 'O';
+public class Day18 extends Day<String> {
 
-    public Day18( Integer... expected ) {super( 18, "Lavaduct Lagoon", expected );}
+    static Pattern p = Pattern.compile( "([RDLU]{1}) ([0-9]+) .#([a-f,0-9]{5})(.)" );
 
-    static final char INSIDE = 'I';
 
-    static final char TRENCH = '#';
-
-    static final char EMPTY = '.';
-
-    static Pattern p = Pattern.compile( "^([RDLU]{1}) ([0-9]{1,2}).*([a-z,0-9]{6})" );
+    public Day18( String... expected ) {super( 18, "Lavaduct Lagoon", expected );}
 
     @Override
-    public Integer part0( Stream<String> input ) {
-        List<DigCommands> digPlan = input.map( Day18::parseFrom ).toList();
-        List<Coordinates> loop = new ArrayList<>();
-        AtomicReference<Coordinates> digger = new AtomicReference<>( new Coordinates( 0, 0 ) );
-        AtomicInteger maxX = new AtomicInteger( 0 );
-        AtomicInteger maxY = new AtomicInteger( 0 );
-        AtomicInteger minX = new AtomicInteger( 0 );
-        AtomicInteger minY = new AtomicInteger( 0 );
-        digPlan.forEach( command -> {
-
-            IntStream.range( 0, command.meters ).forEach( i -> {
-                loop.add( digger.get() );
-                digger.set( digger.get().moveTo( command.d, command.color ) );
-            } );
-            maxX.set( Math.max( maxX.get(), digger.get().getX() ) );
-            maxY.set( Math.max( maxY.get(), digger.get().getY() ) );
-            minX.set( Math.min( minX.get(), digger.get().getX() ) );
-            minY.set( Math.min( minY.get(), digger.get().getY() ) );
-        } );
-        System.out.printf( "%d x %d : %d x %d%n", minX.get(), minY.get(), maxX.get(), maxY.get() );
-        //adding 1 space around the matrix
-        Matrix matrix = new Matrix( maxX.get() - minX.get() + 1 + 2, maxY.get() - minY.get() + 1 + 2, minX.get() - 1, minY.get() - 1 );
-        matrix.setAllValues( EMPTY );
-        loop.forEach( trench -> {
-            matrix.setValue( trench, TRENCH );
-        } );
-        //matrix.setValue( 0, 0, 'S' );
-        flood( matrix, new Coordinates( minX.get() - 1, minY.get() - 1 ) );
-        System.out.println( matrix );
-        return matrix.findValues( EMPTY, false ).size() + matrix.findValues( TRENCH, false ).size();
+    public String part0( Stream<String> input ) {
+        return solve( input, DigCommands::meters, DigCommands::dir );
     }
 
+    @Override
+    public String part1( Stream<String> input ) {
+        return solve( input, DigCommands::hexMeters, DigCommands::hexDir );
+    }
+
+    String solve( Stream<String> input, Function<DigCommands, Integer> meters, Function<DigCommands, Direction> direction ) {
+        List<DigCommands> digPlan = input.map( Day18::parseFrom ).toList();
+        AtomicReference<Coordinates> digger = new AtomicReference<>( new Coordinates( 0, 0 ) );
+        List<Coordinates> vertices = digPlan.stream().map( c -> c.getThere( digger, direction, meters ) ).toList();
+        return pickTheorem( shoelace( vertices ), boundaryPoints( digPlan, meters ) ).toString();
+    }
+
+    //this produces the correct "sequence" for the test input as compared to website
+    static DigCommands parseFrom( String line) {
+        Matcher m = p.matcher( line );
+        if ( m.find() ) {
+            Integer lastHexDigit = Integer.parseInt( m.group( 4 ), 16 );
+            return new DigCommands( getDirection( m.group( 1 ).charAt( 0 ) ),
+                                    Integer.parseInt( m.group( 2 ) ),
+                                    getDirection( lastHexDigit.toString().charAt( 0 ) ),
+                                    Integer.parseInt( m.group( 3 ), 16 ));
+        }
+        throw new IllegalStateException( "Line '" + line + "' could not be parsed" );
+    }
+
+    /**
+     * For reasons, Pick's Theorem which originally goes area = shoelaceresult + (boundary/2) - 1 needs to modified to +1 instead, I figured it out
+     * and then saw someone on reddit attempting a proof of why that is which I couldn't quite follow, but +1 works
+     */
+    BigInteger pickTheorem( BigInteger shoelace, BigInteger boundary ) {
+        return shoelace.add( boundary.divide( BigInteger.TWO ) ).add( BigInteger.ONE );
+    }
+
+    /**
+     * BigInt to protect us from overflows and in general produce safer math
+     */
+    public BigInteger shoelace( List<Coordinates> vertices ) {
+        int numberOfVertices = vertices.size();
+
+        BigInteger sum1 = multiply( vertices.getLast(), vertices.getFirst() );
+        BigInteger sum2 = multiply( vertices.getFirst(), vertices.getLast() );
+
+        for ( int i = 0; i < numberOfVertices - 1; i++ ) {
+            sum1 = sum1.add( multiply( vertices.get( i ), vertices.get( i + 1 ) ) );
+            sum2 = sum2.add( multiply( vertices.get( i + 1 ), vertices.get( i ) ) );
+        }
+
+        return sum1.subtract( sum2 ).abs().divide( BigInteger.TWO );
+    }
+
+    BigInteger boundaryPoints( List<DigCommands> digplan, Function<DigCommands, Integer> mapper ) {
+        return BigInteger.valueOf( ( (Integer) digplan.stream().mapToInt( mapper::apply ).sum() ).longValue() );
+    }
+
+    static Direction getDirection( char indicator ) {
+        return switch ( indicator ) {
+            case 'R', '0' -> Direction.east;
+            case 'D', '1' -> Direction.south;
+            case 'L', '2' -> Direction.west;
+            case 'U', '3' -> Direction.north;
+            default -> throw new IllegalStateException( "Direction '" + indicator + "' not recognized" );
+        };
+    }
+
+    BigInteger multiply( Coordinates forX, Coordinates forY ) {
+        Function<Coordinates, Integer> xF = Coordinates::getX;
+        Function<Coordinates, Integer> yF = Coordinates::getY;
+        return convert( forX, xF ).multiply( convert( forY, yF ) );
+    }
+
+    BigInteger convert( Coordinates c, Function<Coordinates, Integer> coordinateFunc ) {
+        return BigInteger.valueOf( coordinateFunc.apply( c ).longValue() );
+    }
+
+    /**
+     * This solves part 0/1 easily but part 2 would run out of heapspace with the size of the matrix -> still keeping it
+     */
+    @Deprecated
     private void flood( Matrix matrix, Coordinates start ) {
         Stack<Coordinates> nodes = new Stack<>();
         Set<Coordinates> visited = new HashSet<>();
@@ -75,44 +118,29 @@ public class Day18 extends Day<Integer> {
             }
             visited.add( node );
             int value = matrix.getValue( node );
-            if ( value == OUTSIDE ) {
+            if ( value == 'O' ) {
                 //nothing new to see here: we've been here before and added the neighbors
             }
-            else if ( value == INSIDE ) {
+            else if ( value == 'I' ) {
                 //nothing to see here: actually also do nothing? because that is part of the structure we're trying to isolate
             }
-            else if ( value == EMPTY ) {
+            else if ( value == '.' ) {
                 //go get the neighbors:
-                matrix.setValue( node, OUTSIDE );
+                matrix.setValue( node, 'O' );
                 Direction.getBasic().stream().map( node::moveTo ).filter( Predicate.not( visited::contains ) ).toList().forEach( nodes::push );
             }
         }
     }
 
-    static DigCommands parseFrom( String line ) {
-        Matcher m = p.matcher( line );
-        if ( m.find() ) {
-            return new DigCommands( getDirection( m.group( 1 ).charAt( 0 ) ),
-                                    Integer.parseInt( m.group( 2 ) ),
-                                    Integer.parseInt( m.group( 3 ), 16 ) );
+    record DigCommands(Direction dir, int meters, Direction hexDir, int hexMeters) {
+
+        Coordinates getThere( AtomicReference<Coordinates> from,
+                              Function<DigCommands, Direction> dirFunc,
+                              Function<DigCommands, Integer> meterFunc ) {
+            Coordinates diggerCurrent = from.get();
+            Coordinates diggerNext = diggerCurrent.moveTo( dirFunc.apply( this ), meterFunc.apply( this ), 1 );
+            from.set( diggerNext );
+            return diggerNext;
         }
-        throw new IllegalStateException( "Line '" + line + "' could not be parsed" );
     }
-
-    static Direction getDirection( char indicator ) {
-        return switch ( indicator ) {
-            case 'R' -> Direction.east;
-            case 'D' -> Direction.south;
-            case 'L' -> Direction.west;
-            case 'U' -> Direction.north;
-            default -> throw new IllegalStateException( "Direction '" + indicator + "' not recognized" );
-        };
-    }
-
-    @Override
-    public Integer part1( Stream<String> input ) {
-        return null;
-    }
-
-    record DigCommands(Direction d, int meters, int color) {}
 }
