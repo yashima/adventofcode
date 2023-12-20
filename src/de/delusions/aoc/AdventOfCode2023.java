efa2a077e1a8e0a51b5e1283a5d1ccb18c5c8ef0
Sonja Pieper <yashima@users.noreply.github.com>
2023-12-20 20:58:00 +0100

    Day 20: prep + inputs

--- src/de/delusions/aoc/AdventOfCode2023.java
+++ src/de/delusions/aoc/AdventOfCode2023.java
@@ -25,7 +25,8 @@ public class AdventOfCode2023 {
                                              new Day15( 1320, 145, 503487, 261505 ), new Day16( 46, 51, 6921, 7594 ),//
                                              new Day17( 102, 71, 698, 825 ),//
                                              new Day18( "62", "952408144115", "58550", "47452118468566" ),
-                                             new Day19( 19114L, 167409079868000L, 368523L, 124167549767307L ) );
+                                             new Day19( 19114L, 167409079868000L, 368523L, 124167549767307L ),
+                                             new Day20( 11687500L, 0L, 0L, 0L ) );
         System.out.println( "Days: " + daysOfAdvent.size() );
         runAllVariants( daysOfAdvent.getLast() );
 
---
+++ src/de/delusions/aoc/Day20.java
@@ -0,0 +1,19 @@
+package de.delusions.aoc;
+
+import de.delusions.util.Day;
+
+import java.util.stream.Stream;
+
+public class Day20 extends Day<Long> {
+    public Day20( Long... expected ) {super( 20, "Pulse Propagations", expected );}
+
+    @Override
+    public Long part0( Stream<String> input ) {
+        return null;
+    }
+
+    @Override
+    public Long part1( Stream<String> input ) {
+        return null;
+    }
+}
