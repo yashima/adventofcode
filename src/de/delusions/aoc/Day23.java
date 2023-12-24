40533767646a2cb277722fcdc07d69f9579f0b3c
Sonja Pieper <yashima@users.noreply.github.com>
2023-12-24 15:40:42 +0100

    Day 22: solved part 2. Recursion gave me a bit of trouble and a few stackoverflows on my first 2 attempts

---
+++ src/de/delusions/aoc/Day23.java
@@ -0,0 +1,19 @@
+package de.delusions.aoc;
+
+import de.delusions.util.Day;
+
+import java.util.stream.Stream;
+
+public class Day23 extends Day<Integer> {
+    public Day23( Integer... expected ) {super( 23, "A Long Walk", expected );}
+
+    @Override
+    public Integer part0( Stream<String> input ) {
+        return null;
+    }
+
+    @Override
+    public Integer part1( Stream<String> input ) {
+        return null;
+    }
+}
