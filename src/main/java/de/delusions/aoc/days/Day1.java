b53b966be86f29a2abef546df7b64dd266245db1
Sonja Pieper <yashima@users.noreply.github.com>
2024-12-01 16:52:40 +0100

    And part 2 is solved. Some fun streaming shenanigans. Still not pretty.

--- src/main/java/de/delusions/aoc/days/Day1.java
+++ src/main/java/de/delusions/aoc/days/Day1.java
@@ -4,36 +4,52 @@ import de.delusions.util.Day;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Day1 extends Day<String> {
+public class Day1 extends Day<Integer> {
 
     public Day1() {
-        super(1, "Historian Hysteria","11","0" ,"0","0");
+        super(1, "Historian Hysteria",11,31 ,1341714,27384707);
     }
 
-    @Override
-    public String part0(Stream<String> input) {
-        List<Integer> a = new ArrayList<>();
-        List<Integer> b = new ArrayList<>();
-        input.forEach(line -> {
+    record HistoriansHaveBadHandWriting(List<Integer> a, List<Integer> b){
+        void addLine(String line){
             String[] parts = line.split("\\s+");
             a.add(Integer.parseInt(parts[0].trim()));
             b.add(Integer.parseInt(parts[1].trim()));
-        });
-        a.sort(Integer::compareTo);
-        b.sort(Integer::compareTo);
-        int sum = 0;
-        for(int i = 0; i < a.size(); i++) {
-            sum += Math.abs(a.get(i) - b.get(i));
         }
-        return sum+"";
+
+        int size(){
+            return a.size();
+        }
+
+        int countEntries(int i){
+            return (int)b.stream().filter(j -> j == i).count();
+        }
     }
 
     @Override
-    public String part1(Stream<String> input) {
+    public Integer part0(Stream<String> input) {
+        HistoriansHaveBadHandWriting h = new HistoriansHaveBadHandWriting(new ArrayList<>(), new ArrayList<>());
+        input.forEach(h::addLine);
+        h.a.sort(Integer::compareTo);
+        h.b.sort(Integer::compareTo);
+        int sum = 0;
+        for(int i = 0; i < h.size(); i++) {
+            sum += Math.abs(h.a.get(i) - h.b.get(i));
+        }
+        return sum;
+    }
 
-        return "0";
+    @Override
+    public Integer part1(Stream<String> input) {
+        HistoriansHaveBadHandWriting h = new HistoriansHaveBadHandWriting(new ArrayList<>(), new ArrayList<>());
+        input.forEach(h::addLine);
+        Map<Integer, Integer> map = h.a.stream().collect(Collectors.toMap(i -> i, i ->  h.countEntries(i), (i,j)->i + h.countEntries(j) ));
+        return map.keySet().stream().map(i -> map.get(i)*i).mapToInt(Integer::intValue).sum();
+
     }
 }
