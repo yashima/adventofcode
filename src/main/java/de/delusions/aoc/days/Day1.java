854ff122035d7ff12eb3e0b3ce989dac6a54d7c2
Sonja Pieper <yashima@users.noreply.github.com>
2024-12-02 22:13:07 +0100

    Day1: cleaning up imports.

--- src/main/java/de/delusions/aoc/days/Day1.java
+++ src/main/java/de/delusions/aoc/days/Day1.java
@@ -5,7 +5,6 @@ import de.delusions.util.Day;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
