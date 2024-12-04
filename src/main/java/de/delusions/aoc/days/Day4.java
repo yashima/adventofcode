3abda71667d8e066a47d6931de7b63b1fc06ec86
Sonja Pieper <yashima@users.noreply.github.com>
2024-12-04 20:04:01 +0100

    Day4: solved part 1 and 2 with the help of previous years' Matrix implementation. It is nice that chars are just numbers.

---
+++ src/main/java/de/delusions/aoc/days/Day4.java
@@ -0,0 +1,63 @@
+package de.delusions.aoc.days;
+
+import de.delusions.util.Coordinates;
+import de.delusions.util.Day;
+import de.delusions.util.Direction;
+import de.delusions.util.Matrix;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+public class Day4 extends Day<Integer> {
+
+
+    public Day4() {
+        super("Ceres Search", 18, 9, 2454, 0);
+    }
+
+    char[] SEARCH = {'X', 'M', 'A', 'S'};
+
+    @Override
+    public Integer part0(Stream<String> input) {
+        Matrix xmas = Matrix.createFromStream(input);
+        List<Coordinates> x = xmas.findValues('X', false);
+        AtomicInteger sum = new AtomicInteger(0);
+        x.stream().forEach(coords -> {
+            for (Direction dir : Direction.values()) {
+                Coordinates current = coords;//step0
+                boolean foundXmas = true;
+                for (int step = 1; step < SEARCH.length; step++) {
+                    current = current.moveTo(dir, 1);
+                    if (!xmas.isInTheMatrix(current) || xmas.getValue(current) != SEARCH[step]) {
+                        foundXmas = false;
+                        break;
+                    }
+                }
+                if (foundXmas) {
+                    sum.incrementAndGet();
+                }
+            }
+        });
+        return sum.get();
+    }
+
+    @Override
+    public Integer part1(Stream<String> input) {
+        Matrix xmas = Matrix.createFromStream(input);
+        List<Coordinates> a = xmas.findValues('A', false);
+        AtomicInteger sum = new AtomicInteger(0);
+        a.stream().forEach(coords -> {
+            Coordinates ne = coords.moveTo(Direction.northeast);
+            Coordinates sw = coords.moveTo(Direction.southwest);
+            Coordinates se = coords.moveTo(Direction.southeast);
+            Coordinates nw = coords.moveTo(Direction.northwest);
+            if (xmas.isInTheMatrix(ne, sw, se, nw)) {
+                if (xmas.getValue(ne) + xmas.getValue(sw) == 'M' + 'S' && xmas.getValue(se) + xmas.getValue(nw) == 'M' + 'S') {
+                    sum.incrementAndGet();
+                }
+            }
+        });
+        return sum.get();
+    }
+}
