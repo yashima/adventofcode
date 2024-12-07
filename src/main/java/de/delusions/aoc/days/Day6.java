81cb119fa9f1cbc4fc118134a41c5a0c4dad247e
Sonja Pieper <yashima@users.noreply.github.com>
2024-12-07 09:10:52 +0100

    Day 6: fixed a bug, the guard needs to turn again if there is another obstacle, only begin moving once the next thing is definitly not an obstacle. Gained me 3 cycles but not the correct result.

--- src/main/java/de/delusions/aoc/days/Day6.java
+++ src/main/java/de/delusions/aoc/days/Day6.java
@@ -7,12 +7,16 @@ import de.delusions.util.Matrix;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class Day6 extends Day<Integer> {
     private static final Logger LOG = LoggerFactory.getLogger(Day6.class);
+
     public Day6() {
         super("", 41, 6, 4982, 0);
         Coordinates.USE_FACING = true;
@@ -32,42 +36,43 @@ public class Day6 extends Day<Integer> {
 
     private List<Coordinates> trackTheGuard(Matrix warehouse, Coordinates guard, Function<Direction, Character> visitor) {
         List<Coordinates> steps = new ArrayList<>();
-        while (warehouse.isInTheMatrix(guard)) {
+        while (warehouse.isInTheMatrix(guard)) { //while he is in the room
 
-            if (visitor != null) {
+            if (visitor != null) { //only store visitor steps if you know how
                 warehouse.setValue(guard, visitor.apply(guard.getFacing()));
             }
 
-            if (warehouse.getValue(guard.moveToNext(),-1) == OBSTACLE) {
+            if (warehouse.getValue(guard.moveToNext(), -1) == OBSTACLE) { //test next move for obstacles
                 guard.setFacing(guard.getFacing().turnRight());//guard turns before the obstacle
-                if(steps.contains(guard)){//only check for cycles before an obstacle
+                if (steps.contains(guard)) {//only check for cycles before an obstacle, it is much faster and gets same results as all steps
                     throw new IllegalStateException("Cycle detected");
                 }
+            } else {
+                steps.add(guard);//add with the turn
+                guard = guard.moveToNext();
             }
-            steps.add(guard);//add with the turn
-            guard = guard.moveToNext();
         }
         return steps;
     }
 
-    //1598 too low, 2450 too high, 1743 not right
+    //1598 too low, 2450 too high, 1743, 1796 not right not right
     @Override
     public Integer part1(Stream<String> input) {
         Matrix warehouse = Matrix.createFromStream(input);
         Coordinates guard = warehouse.findValues('^', true).getFirst();
         guard.setFacing(Direction.north);
-        List<Coordinates> steps = trackTheGuard(warehouse, guard,null);
+        List<Coordinates> steps = trackTheGuard(warehouse, guard, null); //get all original steps
         Set<Coordinates> loops = new HashSet<>();
         for (Coordinates step : steps) {
-            Coordinates candidate = step.moveToNext();
-            if (warehouse.isInTheMatrix(candidate) && warehouse.getValue(candidate) != OBSTACLE && !loops.contains(candidate)) {
-                warehouse.setValue(candidate, OBSTACLE);
+            Coordinates candidate = step.moveToNext(); //try to block next step of the guard
+            if (warehouse.isInTheMatrix(candidate) ) { //double test no issue, and since we turn before obstacle that doesn't happen
+                warehouse.setValue(candidate, OBSTACLE); //temporarily add obstacle
                 try {
-                    trackTheGuard(warehouse, step, null);
+                    trackTheGuard(warehouse, step, null); //check circles
                 } catch (IllegalStateException e) {
-                    loops.add(candidate);
+                    loops.add(candidate); //add candidate to set
                 }
-                warehouse.setValue(candidate, '.');
+                warehouse.setValue(candidate, '.'); //remove tested obstacle
             }
         }
         return loops.size();
