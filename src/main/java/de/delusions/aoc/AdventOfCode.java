d6656daa78a4240744650c9a53663c15969e5874
Sonja Pieper <yashima@users.noreply.github.com>
2024-12-07 10:11:20 +0100

    Tools: removed imports from AoC main class

--- src/main/java/de/delusions/aoc/AdventOfCode.java
+++ src/main/java/de/delusions/aoc/AdventOfCode.java
@@ -1,14 +1,8 @@
 package de.delusions.aoc;
 
-import de.delusions.aoc.days.Day1;
-import de.delusions.aoc.days.Day2;
-import de.delusions.aoc.days.Day3;
 import de.delusions.util.Day;
 
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
 public class AdventOfCode {
     public static void main(String[] args) {
