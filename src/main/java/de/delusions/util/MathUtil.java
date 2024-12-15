96efe4501d74ea2803fcff78284e95a505ee6bf7
Sonja Pieper <yashima@users.noreply.github.com>
2024-12-15 11:03:26 +0100

    Tools: including standard deviation function in my math utils

--- src/main/java/de/delusions/util/MathUtil.java
+++ src/main/java/de/delusions/util/MathUtil.java
@@ -2,6 +2,7 @@ package de.delusions.util;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MathUtil {
@@ -45,6 +46,47 @@ public class MathUtil {
             result = result.multiply(BigInteger.valueOf(factor));
         }
         return result;
+    }
+
+    /**
+     * Calculates the standard deviation of the provided values.
+     * See: <a href="https://rosettacode.org/wiki/Cumulative_standard_deviation#Java">Standard Deviation</a>
+     *
+     * @param values the array of double values
+     * @return the standard deviation of the values
+     */
+    public static double sd(double[] values) {
+        int n = 0;
+        double sum = 0;
+        double squares = 0;
+        for (double value : values) {
+            n++;
+            sum += value;
+            squares += value * value;
+        }
+        return Math.sqrt(squares / n - sum * sum / n / n);
+    }
+
+    /**
+     * Calculates the median of the provided values.
+     * See: <a href="https://rosettacode.org/wiki/Averages/Median#Java">Median Calculation</a>
+     *
+     * @param values the array of double values
+     * @return the median of the values
+     */
+    public static double median(double[] values) {
+        List<Double> list = Arrays.stream(values).boxed().toList();
+        list.sort(Double::compareTo);
+        int mid = list.size() / 2;
+        return switch (list.size() % 2) {
+            case 0 -> {
+                double valueA = list.get(mid);
+                double valueB = list.get(mid + 1);
+                yield (valueA + valueB) / 2;
+            }
+            case 1 -> list.get(mid);
+            default -> 0;
+        };
     }
 
 }
