acb9774edb40344859f036c3cb1192b32cbfaa5b
Sonja Pieper <yashima@users.noreply.github.com>
2024-12-20 00:54:14 +0100

    Day 19: part 2 solved. backwards thinking and memoization. Meh.

--- src/main/java/de/delusions/aoc/advent2024/Day19.java
+++ src/main/java/de/delusions/aoc/advent2024/Day19.java
@@ -5,93 +5,81 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Day19 extends Day<Integer> {
+public class Day19 extends Day<Long> {
 
     private static final Logger LOG = LoggerFactory.getLogger(Day19.class);
 
+    private static Pattern metaPattern;
+
+    List<String> towels;
+    Map<String, Memo> cache = new HashMap<>();
+
     public Day19() {
-
-        super("", 6, 16,342, 0);
+        super("Linen Layout", 6L, 16L, 342L, 0L);
     }
 
     @Override
-    public Integer part0(Stream<String> input) {
-        List<String> patterns = input.collect(Collectors.toList());
-        List<String> towels = Arrays.stream(patterns.get(0).split(",")).map(String::trim).toList();
-        Pattern metaPattern = Pattern.compile(String.format("(%s)+", towels.stream().collect(Collectors.joining("|"))));
-        return patterns.stream().skip(2).filter(line -> metaPattern.matcher(line).matches()).toList().size();
+    public Long part0(Stream<String> input) {
+        List<String> patterns = input.toList();
+        towels = Arrays.stream(patterns.getFirst().split(",")).map(String::trim).toList();
+        metaPattern = Pattern.compile(String.format("(%s)+", String.join("|", towels)));
+        return (long)patterns.stream().skip(2).filter(Day19::isValid).toList().size();
     }
 
 
     @Override
-    public Integer part1(Stream<String> input) {
-        CACHE.clear();
-        List<String> patterns = input.collect(Collectors.toList());
-        List<String> towels = Arrays.stream(patterns.get(0).split(",")).map(String::trim).toList();
-        Pattern metaPattern = Pattern.compile(String.format("(%s)+", towels.stream().collect(Collectors.joining("|"))));
+    public Long part1(Stream<String> input) {
+        cache.clear();
+        List<String> patterns = input.toList();
+        towels = Arrays.stream(patterns.getFirst().split(",")).map(String::trim).toList();
+        metaPattern = Pattern.compile(String.format("(%s)+", String.join("|", towels)));
         return patterns.stream()
                 .skip(2)
-                .filter( line -> metaPattern.matcher(line).matches())
-                .mapToInt(line -> countVariants(towels, line))
+                .mapToLong(line -> countVariants(line))
+                .sum();
+    }
+
+    static boolean isValid(String line) {
+        return line.isEmpty() || metaPattern.matcher(line).matches();
+    }
+
+    Long countVariants(Object key) {
+        String suffix = (String)key;
+        if (suffix.isEmpty()) { return 1L; }
+        return towels.stream()
+                .filter(suffix::startsWith)
+                .filter(towel -> isValid(suffix.substring(towel.length())))
+                .map(towel -> suffix.substring(towel.length()))
+                .mapToLong(newSuffix -> cache
+                        .computeIfAbsent(newSuffix, Memo::new)
+                        .compute(this::countVariants))
                 .sum();
     }
 
 
-    static Map<String,Set<String>> CACHE = new HashMap<>();
+    //MEMO CACHE KEY -> need to write my own memoization abstraction
+    //^^ this comment is for future reference in full text search!
+    //inspired by: https://github.com/dashie/AdventOfCode2024/blob/main/src/main/java/adventofcode/commons/MemoizationCache.java
+    class Memo<K,V> {
+        K key;
 
-    record Variant(String start, String towelsUsed, String patternEnd){
-        static List<Variant> extendFrom(String key, Variant variant){
-            return CACHE.get(key).stream()
-                    .map(entry -> new Variant(variant.start+key,
-                            String.format("%s%s%s",variant.towelsUsed, variant.towelsUsed=="" ? "" : "-", entry),
-                            variant.patternEnd.substring(key.length())))
-                    .toList();
+        long value;
+
+        Memo(K key) {
+            this.key = key;
+            this.value = 0;
         }
-
-    }
-
-    int countVariants(List<String> towels, String pattern){
-        towels.forEach(t -> addToCache(t,t));
-        Set<Variant> good = new HashSet<>();
-        Set<Variant> visited = new HashSet<>();
-        Stack<Variant> stack = new Stack<>();
-        stack.push(new Variant("","", pattern));
-        while(!stack.isEmpty()){
-            Variant current = stack.pop();
-            visited.add(current);
-            if (current.patternEnd.isEmpty()){
-                good.add(current);
-            } else {
-                getNext(towels, current).forEach(v -> {
-                    if (!visited.contains(v)) {
-                        stack.push(v);
-                    }
-                });
+        long compute(Function<K,Long> comp) {
+            if (value == 0) {
+                value = comp.apply(key);
             }
-
+            return value;
         }
-        System.out.println("Finished "+pattern);
-        return good.size();
-    }
 
-    void addToCache(String start,String towelsUsed){
-        CACHE.putIfAbsent(start, new HashSet<>());
-        CACHE.get(start).add(towelsUsed);
-    }
-
-
-    List<Variant> getNext(List<String> towels, Variant variant){
-        List<Variant> list = CACHE.keySet().stream()
-                .filter(key -> variant.patternEnd.startsWith(key))
-                .map(key -> Variant.extendFrom(key, variant))
-                .flatMap(List::stream)
-                .toList();
-        list.stream().forEach(v -> addToCache(v.start,v.towelsUsed));
-        return list;
     }
 
 
