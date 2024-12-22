e5ad11be441e28b853bf81bf77defb399a9d819c
Sonja Pieper <yashima@users.noreply.github.com>
2024-12-22 01:33:53 +0100

    Day 21: solved part 1, part 2 too slow despite being configurable. needs more memoization apparently. yikes

--- src/main/java/de/delusions/aoc/advent2024/Day21.java
+++ src/main/java/de/delusions/aoc/advent2024/Day21.java
@@ -13,19 +13,26 @@ public class Day21 extends Day<Long> {
     private static final Logger LOG = LoggerFactory.getLogger(Day21.class);
 
     public Day21() {
-        super("", 126384L, 0L, 0L, 0L);
+        super("", 126384L, 0L, 203734L, 0L);
     }
 
     @Override
     public Long part0(Stream<String> input) {
-        //205038 -> too high | 208147
-        //incomplete. solution fails to deliver optimal paths.
-        return input.map(RobotsAllTheWayDown::new).map(RobotsAllTheWayDown::pressSomeButtons).mapToLong(State::complexity).sum();
+        return input
+                .map(RobotsAllTheWayDown::new)
+                .map(RobotsAllTheWayDown::pressSomeButtons)
+                .mapToLong(State::complexity)
+                .sum();
     }
 
     @Override
     public Long part1(Stream<String> input) {
-        return 0L;
+        //TODO write another f...ing memoization cache
+        return input
+                .map(code -> new RobotsAllTheWayDown(code,26))
+                .map(RobotsAllTheWayDown::pressSomeButtons)
+                .mapToLong(State::complexity)
+                .sum();
     }
 
 
@@ -42,23 +49,30 @@ public class Day21 extends Day<Long> {
     }
 
     static class RobotsAllTheWayDown {
-        static int MAX_LEVEL = 3;
         Map<String, String> cursorMap = new HashMap<>();
         Map<String, String> numPad = new HashMap<>();
-        Map<String, String>[] metaMap = new Map[MAX_LEVEL];
-        char[] positions = "A".repeat(MAX_LEVEL).toCharArray();
+        Map<String, String>[] metaMap;
+        char[] positions;
+        int maxLevel;
         State state;
 
         RobotsAllTheWayDown(String code) {
+            this(code,3);
+        }
+
+        RobotsAllTheWayDown(String code, int maxLevel) {
             this.state = State.createFromCode(code);
+            this.maxLevel = maxLevel;
+            metaMap = new Map[maxLevel];
+            positions = "A".repeat(maxLevel).toCharArray();
             hardCodedMapsInitializationMagic();
         }
 
         State pressSomeButtons() {
-            while (state.level < MAX_LEVEL) {
+            while (state.level < maxLevel) {
                 StringBuilder sb = new StringBuilder();
                 String target = state.target;
-                LOG.debug("Level {} with target {}", state.level, target);
+                //LOG.debug("Level {} with target {}", state.level, target);
                 while (target.length() > 0) {
                     char position = positions[state.level];
                     char nextChar = target.charAt(0);
@@ -72,7 +86,7 @@ public class Day21 extends Day<Long> {
                 }
                 state = new State(state.number, sb.toString(), "", state.level + 1);
             }
-            LOG.debug("Level {} with target {}", state.level, state.target);
+            //LOG.debug("Level {} with target {}", state.level, state.target);
             return state;
         }
 
@@ -84,7 +98,7 @@ public class Day21 extends Day<Long> {
             cursorMap.put("A:A", "");
 
             cursorMap.put("^:A", ">");
-            cursorMap.put("^:v", "v");
+            //cursorMap.put("^:v", "v");
             cursorMap.put("^:>", "v>");
             cursorMap.put("^:<", "v<");
             cursorMap.put("^:^", "");
@@ -93,16 +107,16 @@ public class Day21 extends Day<Long> {
             cursorMap.put(">:A", "^");
             cursorMap.put(">:^", "<^");
             cursorMap.put(">:v", "<");
-            cursorMap.put(">:<", "<<");
+            //cursorMap.put(">:<", "<<");
 
             cursorMap.put("v:A", ">^");
             cursorMap.put("v:>", ">");
             cursorMap.put("v:<", "<");
-            cursorMap.put("v:^", "^");
+            //cursorMap.put("v:^", "^");
             cursorMap.put("v:v", "");
 
             cursorMap.put("<:v", ">");
-            cursorMap.put("<:>", ">>");
+            //cursorMap.put("<:>", ">>");
             cursorMap.put("<:A", ">>^");
             cursorMap.put("<:^", ">^");
             cursorMap.put("<:<", "");
@@ -117,13 +131,10 @@ public class Day21 extends Day<Long> {
             numPad.put("9:8", "<");
             numPad.put("8:0", "vvv");
             numPad.put("0:A", ">");
-
             //ex3
             numPad.put("A:1", "^<<");
             numPad.put("1:7", "^^");
             numPad.put("7:9", ">>");
-            //9:A
-
             //ex4
             numPad.put("A:4", "^^<<");
             numPad.put("4:5", ">");
@@ -135,12 +146,13 @@ public class Day21 extends Day<Long> {
 
             //083a
             numPad.put("0:8", "^^^");
-            numPad.put("8:3", ">vv");
+            numPad.put("8:3", "vv>");
             numPad.put("3:A", "v");
+
             //935a
             numPad.put("9:3", "vv");
-            numPad.put("3:5", "^<");
-            numPad.put("5:A", ">vv");
+            numPad.put("3:5", "<^");
+            numPad.put("5:A", "vv>");
             //964A
             numPad.put("9:6", "v");
             numPad.put("6:4", "<<");
@@ -155,7 +167,7 @@ public class Day21 extends Day<Long> {
             numPad.put("8:9", ">");
 
             metaMap[0] = numPad;
-            for (int i = 1; i < MAX_LEVEL; i++) {
+            for (int i = 1; i < maxLevel; i++) {
                 metaMap[i] = cursorMap;
             }
         }
