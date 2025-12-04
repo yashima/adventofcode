75d2f8f97ca5843580c34af07e2bb28c0d740722
yashima <github@delusions.de>
2025-12-04 15:11:39 +0100

    2025-DAY3 Cleanup imports.

--- src/main/test/de/delusions/aoc/advent2025/Day03Test.java
+++ src/main/test/de/delusions/aoc/advent2025/Day03Test.java
@@ -5,9 +5,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.testng.Assert.*;
 
 @Slf4j
 public class Day03Test {
