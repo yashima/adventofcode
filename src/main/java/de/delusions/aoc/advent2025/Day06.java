d28ca3967e4f5274434d68cbb2834394c9e7e436
yashima <github@delusions.de>
2025-12-06 14:57:56 +0100

    2025-DAY6 re.  So I solved the second part again in a more elegant way or so I think. Back to using regex for parsing. Learned something new.

--- src/main/java/de/delusions/aoc/advent2025/Day06.java
+++ src/main/java/de/delusions/aoc/advent2025/Day06.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -16,7 +15,8 @@ import java.util.stream.Stream;
  * Day 6: Trash Compactor.
  * Part 1 took me longer than it strictly had to, but I feel the need to use proper object-oriented programming.
  * I find it makes my code more readable and easier to maintain.
- * Part 2: is a parsing problem.
+ * Part 2: is a parsing problem. After solving it, I realized I could have transposed the whole input for an even easier solution.
+ * And I can also get back to using regular expressions to help with the parsing: yay!
  */
 @Slf4j
 public class Day06 extends Day<Long> {
@@ -33,64 +33,82 @@ public class Day06 extends Day<Long> {
     }
 
     /** Represents one of the cephalopod math problems to solve */
-    record Problem(List<String> operands, Operator op){
+    record Problem(List<Long> operands, Operator op){
 
-        static Problem create(List<String> operands,char op){
+        /** I am parsing the input into a matrix for part 1 */
+        static Problem create(int[] column){
+            return new Problem(
+                    Arrays.stream(column).limit(column.length-1).mapToLong(i -> Long.valueOf(i)).boxed().toList(),
+                    Operator.fromChar((char)column[column.length-1]));
+        }
+
+        /** For part 2 I have already created a list of numbers */
+        static Problem createFromLong(List<Long> operands,char op){
             return new Problem(operands,Operator.fromChar(op));
         }
 
-        /** Solve the problem either by adding or multiplying the operands */
+        /** Now solve the problem and return the sum or product */
         Long solve(){
-           Matrix m = Matrix.createFromStream(operands.stream());
-           List<Long> opsAsNumbers = m.rows().map(Matrix::toCharString).filter(Predicate.not(String::isBlank)).mapToLong(l -> Long.parseLong(l.trim())).boxed().toList();
-           return solve(opsAsNumbers);
-        }
-
-        private Long solve(List<Long> operandsParam){
             if (op == Operator.ADD) {
-                return operandsParam.stream().reduce(0L, (a, b) -> a + b);
+                return operands.stream().reduce(0L, (a, b) -> a + b);
             } else {
-                return operandsParam.stream().reduce(1L, (a, b) -> a * b);
+                return operands.stream().reduce(1L, (a, b) -> a * b);
             }
         }
 
-        Long solveVertical(){
-            Matrix m = Matrix.createFromStream(operands.stream());
-            List<Long> opsAsNumbers = m.transposeLeft().rows().map(Matrix::toCharString).filter(Predicate.not(String::isBlank)).mapToLong(l -> Long.parseLong(l.trim())).boxed().toList();
-            return solve(opsAsNumbers);
-        }
     }
 
-    @Override
-    public Long part0(Stream<String> input) {
-        return createProblems(input).stream().mapToLong(Problem::solve).sum();
+    static Pattern lineParserRegex = Pattern.compile("(\\d+)|([+*])");
+
+    /**
+     * We actually need to do some parsing today, as there are lines that are formatted differently than the others.
+     * @param line lines can contain either numbers split by spaces or a single operator.
+     * @return an array of integers representing the parsed line for us in a matrix, operators are represented by their char value
+     */
+    public static int[] parseLine(String line){
+        Matcher matcher = lineParserRegex.matcher(line);
+        return matcher.results().mapToInt(m -> {
+            if (m.group(2) != null) {
+                return m.group(2).charAt(0);
+            } else {
+                return Integer.parseInt(m.group(1));
+            }
+        }).toArray();
     }
 
-    static List<Problem> createProblems(Stream<String> input) {
-        List<String> lines = input.toList();
-        String operators = lines.getLast();
+    static Pattern lineParserRegex2 = Pattern.compile("(\\d+)\\s*([+*]?)");
+
+    static List<Problem> createTransposedProblems(List<String> lines){
+        List<String> linesTransposed = Matrix.createFromStream(lines.stream()).transposeLeft().rows().map(Matrix::toCharString).toList();
         List<Problem> problems = new ArrayList<>();
-        int problemStartIdx = 0;
-        for(int pos=0;pos<operators.length()-1;pos++){
-            if(List.of('+','*').contains(operators.charAt(pos))) {
-                if(pos>0){
-                    int finalProblemStartIdx = problemStartIdx;
-                    int finalPos = pos;
-                    List<String> operands = lines.stream().limit(lines.size() - 1).map(l -> l.substring(finalProblemStartIdx, finalPos)).toList();
-                    problems.add(Problem.create(operands,operators.charAt(problemStartIdx)));
-                    problemStartIdx = pos;
+        List<Long> operands = new ArrayList<>();
+
+        linesTransposed.forEach(l ->{
+            Matcher matcher = lineParserRegex2.matcher(l.trim());
+            if(matcher.matches()){ //ignores empty lines
+                operands.add(Long.parseLong(matcher.group(1)));
+                if(matcher.group(2)!=null && !matcher.group(2).isBlank()){
+                    char op = matcher.group(2).charAt(0);
+                    problems.add(Problem.createFromLong(new ArrayList<>(operands), op));
+                    operands.clear();
                 }
-            } else if (pos==operators.length()-2){
-                int finalProblemStartIdx = problemStartIdx;
-                List<String> operands = lines.stream().limit(lines.size() - 1).map(l -> l.substring(finalProblemStartIdx)).toList();
-                problems.add(Problem.create(operands,operators.charAt(problemStartIdx)));
             }
-        }
+        });
         return problems;
     }
 
     @Override
+    public Long part0(Stream<String> input) {
+        return Matrix
+                .createFromStream(input,Day06::parseLine)
+                .columns()
+                .map(Problem::create)
+                .mapToLong(Problem::solve)
+                .sum();
+    }
+
+    @Override
     public Long part1(Stream<String> input) {
-        return createProblems(input).stream().mapToLong(Problem::solveVertical).sum();
+        return createTransposedProblems(input.toList()).stream().mapToLong(Problem::solve).sum();
     }
 }
--- src/main/test/de/delusions/aoc/advent2025/Day06Test.java
+++ src/main/test/de/delusions/aoc/advent2025/Day06Test.java
@@ -3,7 +3,6 @@ package de.delusions.aoc.advent2025;
 import de.delusions.util.Matrix;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -11,56 +10,63 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.regex.Matcher;
 
 @Slf4j
 public class Day06Test {
 
-    static String inputAsString  = """
-            123 328  51 64
-            45 64  387 23
-            6 98  215 314
-            *   +   *   +
-            """;
+    static String inputAsString  = "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  ";
 
     static int problemId = 0;
 
-    static List<Day06.Problem> problems;
+    static List<Day06.Problem> part1;
+    static List<Day06.Problem> part2;
 
     @BeforeAll
     public static void init(){
-        Stream<String> input = Arrays.stream(inputAsString.split("\n"));;
-        problems = Matrix.createFromStream(input,Day06::parseLine).columns().map(Day06.Problem::create).toList();
-    }
-
-
-    @ParameterizedTest
-    @ValueSource(ints = {33210,490,4243455,401})
-    void testExampleInput(int expected){
-        Day06.Problem problem = problems.get(problemId);
-        Assertions.assertThat(problem.solve()).isEqualTo(expected);
+        part1 = Matrix.createFromStream(Arrays.stream(inputAsString.split("\n")),Day06::parseLine).columns().map(Day06.Problem::create).toList();
+        part2 = Day06.createTransposedProblems(Arrays.stream(inputAsString.split("\n")).toList());
     }
 
     @ParameterizedTest
     @CsvSource({
-            "0,356,24,1",
-            "1,8,248,369",
-            "2,175,581,32",
-            "3,4,431,623"
+            "0,4,431,623 ",
+            "1,175,581,32",
+            "2,8,248,369 ",
+            "3,356,24,1  ",
     })
     void testTranformation(int problemId,int expectedX,int expectedY,int expectedZ){
-        List<Long> transformed = problems.get(problemId).transformed();
-        Assertions.assertThat(transformed.get(0)).isEqualTo(expectedX);
-        Assertions.assertThat(transformed.get(1)).isEqualTo(expectedY);
-        Assertions.assertThat(transformed.get(2)).isEqualTo(expectedZ);
+        List<Long> operands = part2.get(problemId).operands();
+        Assertions.assertThat(operands.get(0)).isEqualTo(expectedX);
+        Assertions.assertThat(operands.get(1)).isEqualTo(expectedY);
+        Assertions.assertThat(operands.get(2)).isEqualTo(expectedZ);
     }
 
-    @AfterEach
-    public void updateId(){
+    @ParameterizedTest
+    @ValueSource(ints = {33210,490,4243455,401})
+    void testExampleInput(int expected){
+        Day06.Problem problem = part1.get(problemId);
+        Assertions.assertThat(problem.solve()).isEqualTo(expected);
         problemId++;
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"542","1+","22","3","52*"})
+    public void testRegexPart2(String in){
+        Matcher matcher = Day06.lineParserRegex2.matcher(in);
+        Assertions.assertThat(matcher.matches()).isTrue();
+        Assertions.assertThat(Long.parseLong(matcher.group(1))).isGreaterThan(0);
+        if(!matcher.group(2).isBlank()){
+            Assertions.assertThat(matcher.group(2).charAt(0)).isIn('+','*');
+        }
+    }
+
+    @Test
+    void testAlternativePart2(){
+        List<Day06.Problem> problems = Day06.createTransposedProblems(Arrays.stream(inputAsString.split("\n")).toList());
     }
 
 }
